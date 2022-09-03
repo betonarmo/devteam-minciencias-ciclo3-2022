bbff2f0a3dbbfdb0da1bfb8b90e36d3ca0047799
Alejandra Tribin <tribinpalacios@gmail.com>
2022-09-03 15:44:24 -0500

    creacion Jugador Alejandra-Rene

---
+++ torneo_futbol/TorneoFutbol.App/TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioJugador.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Torneo.App.Dominio;
+namespace Torneo.App.Persistencia
+{
+    public class RepositorioJugador : IRepositorioJugador
+    {
+        private readonly DataContext _dataContext = new DataContext();
+
+        public Jugador AddJugador(Jugador jugador, int idEquipo, int idPosicion)
+        {
+            var equipoEncontrado = _dataContext.Equipos.Find(idEquipo);
+            var posicionEncontrado = _dataContext.Posiciones.Find(idPosicion);
+            jugador.Equipo = equipoEncontrado;
+            jugador.Posicion = posicionEncontrado;
+            var jugadorInsertado = _dataContext.Jugadores.Add(jugador);
+            _dataContext.SaveChanges();
+            return jugadorInsertado.Entity;
+        }
+       
+    }
+}
