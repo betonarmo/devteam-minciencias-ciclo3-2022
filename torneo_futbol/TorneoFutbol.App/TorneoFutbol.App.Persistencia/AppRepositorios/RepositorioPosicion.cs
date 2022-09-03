8d1cd5b478b05fc32f9bd658ece8097db13c428b
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2022-09-03 14:20:29 -0500

    Creacion de posicion - Alejandra, Rene

---
+++ torneo_futbol/TorneoFutbol.App/TorneoFutbol.App.Persistencia/AppRepositorios/RepositorioPosicion.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Torneo.App.Dominio;
+namespace Torneo.App.Persistencia
+{
+    public class RepositorioPosicion : IRepositorioPosicion
+    {
+        private readonly DataContext _dataContext = new DataContext();
+        public Posicion AddPosicion(Posicion posicion)
+        {
+            var posicionInsertado = _dataContext.Posicions.Add(posicion); 
+            _dataContext.SaveChanges();
+            return posicionInsertado.Entity;
+        }
+    }
+}
